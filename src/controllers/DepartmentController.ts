05c8ff85bd2454e0fdd027928a731bf80d9b69a3
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-10-18 14:18:32 -0300

    feat: get department by department route

--- src/controllers/DepartmentController.ts
+++ src/controllers/DepartmentController.ts
@@ -32,6 +32,20 @@ export class DepartmentController {
     }
   };
 
+  public getDepartmentsByDepartment = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    try {
+      const department = req.params.department;
+      const departments =
+        await this.departmentService.getDepartmentByDepartment(department);
+      res.json(departments);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
   public createDepartment = async (
     req: Request,
     res: Response
