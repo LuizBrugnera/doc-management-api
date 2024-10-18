7fd3feaffde1b7f39b33959bf5d6cd78ce58e516
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-10-18 14:35:59 -0300

    fix: env db variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,17 @@
 import { DataSource } from "typeorm";
 import { join } from "path";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import dotenv from "dotenv";
 
+dotenv.config();
 const dir = __dirname;
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "123456",
-  database: "documents-db",
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [join(dir, "entities", "*.{js,ts}")],
   migrations: [join(dir, "migrations", "*.{js,ts}")],
   synchronize: false,
