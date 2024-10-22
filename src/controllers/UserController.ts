57dfd04480d000b36c5d07892f5a72b3209b8201
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-10-22 19:14:45 -0300

    feat: small auth to user alter only your data

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -39,7 +39,14 @@ export class UserController {
   public updateUser = async (req: Request, res: Response): Promise<void> => {
     try {
       const id = parseInt(req.params.id);
+
+      if (id !== req.user?.id && req.user?.role !== "admin") {
+        res.status(401).json({ message: "Não Autorizado!" });
+        return;
+      }
+
       const userData = req.body;
+
       const user = await this.userService.updateUser(id, userData);
       if (!user) {
         res.status(404).json({ message: "Usuário não encontrado" });
