ded4d098676d9794712876e2fa3411025d2933e7
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2025-06-10 14:56:36 -0300

    feat: update status by assigned

--- src/services/AssignedDocumentService.ts
+++ src/services/AssignedDocumentService.ts
@@ -1,9 +1,11 @@
 import { AppDataSource } from "../data-source";
 import { AssignedDocument } from "../entities/AssignedDocument";
+import { Os } from "../entities/Os";
 
 export class AssignedDocumentService {
   private assignedDocumentRepository =
     AppDataSource.getRepository(AssignedDocument);
+  private osRepository = AppDataSource.getRepository(Os);
 
   async getAllAssignedDocuments(): Promise<AssignedDocument[]> {
     return await this.assignedDocumentRepository.find({
@@ -33,12 +35,32 @@ export class AssignedDocumentService {
     id: number,
     data: Partial<AssignedDocument>
   ): Promise<AssignedDocument | null> {
-    const assignedDocument = await this.assignedDocumentRepository.findOneBy({
-      id,
+    const assignedDocument = await this.assignedDocumentRepository.findOne({
+      where: { id },
+      relations: ["os"],
     });
+
     if (!assignedDocument) {
       return null;
     }
+
+    if (data.status === "assinado") {
+      const os = await this.osRepository.findOneBy({
+        id: assignedDocument.os?.id,
+      });
+
+      const allAssignedByOs = await this.assignedDocumentRepository.find({
+        where: { os: { id: os?.id } },
+      });
+
+      if (allAssignedByOs.every((doc) => doc.status === "assinado")) {
+        if (os) {
+          os.status = "assinado";
+          await this.osRepository.save(os);
+        }
+      }
+    }
+
     this.assignedDocumentRepository.merge(assignedDocument, data);
     return await this.assignedDocumentRepository.save(assignedDocument);
   }
